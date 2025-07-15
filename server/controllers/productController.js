39f0e5830189a556623c3628c3779ce10b667f1f
ATarjun007 <atarjun2000@gmail.com>
2025-07-15 14:31:18 +0530

    product api fixed

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -4,18 +4,21 @@ import { v2 as cloudinary } from "cloudinary";
 //Add Product: /api/product/add
 export const addProduct = async (req, res) => {
   try {
-    let productData = JSON.parse(req.body.productData);
+    let productData = req.body
 
     const images = req.files;
 
-    let imagesUrl = await Promise.all(
-      images.map(async (item) => {
-        let result = await cloudinary.uploader.upload(item.path, {
-          resource_type: "image",
-        });
-        return result.secure_url;
-      })
-    );
+    let imagesUrl = []; // Initialize as empty array to avoid errors if no files
+    if (images && images.length > 0) {
+      imagesUrl = await Promise.all(
+        images.map(async (item) => {
+          let result = await cloudinary.uploader.upload(item.path, {
+            resource_type: "image",
+          });
+          return result.secure_url;
+        })
+      );
+    }
     await Product.create({ ...productData, image: imagesUrl });
     res.json({ success: true, message: "Product added" });
   } catch (error) {
